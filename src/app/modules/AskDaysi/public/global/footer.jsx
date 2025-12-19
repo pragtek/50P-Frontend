d0a2aea3fc8441422da8117913f91548e6e7ea26
PrajwalYakso123 <prajjwalyakso@gmail.com>
2025-12-19 15:40:09 +0545

    Revert "Course module updated"
    This reverts commit 166a7c1d37e459d711b543e0326c1d0663bab810.

--- src/app/modules/AskDaysi/public/global/footer.jsx
+++ src/app/modules/AskDaysi/public/global/footer.jsx
@@ -6,7 +6,6 @@ import {
   ADFooterWrapper,
 } from "../../styles/global/footer";
 import AskDaysiAbout from "../dialogs/about";
-import AskDaysiCourses from "../dialogs/course";
 import CopyrightOutlinedIcon from "@mui/icons-material/CopyrightOutlined";
 import AskDaysiTermsOfService from "../dialogs/TermsOfService";
 import { useTranslation } from "react-i18next";
@@ -36,7 +35,7 @@ const Footer = () => {
               <CopyrightOutlinedIcon
                 sx={{ position: "relative", top: "4px", fontSize: "18px" }}
               />{" "}
-              {year} 50 Paisa Course Dashboard . All rights reserved.
+              {year} TileFlexAI. All rights reserved.
             </ADFooterCopy>
           </Stack>
           <Stack>
